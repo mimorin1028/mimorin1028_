7890c5110d8c0cff7964537d88c2e2ce37cceffc
朱廷果 <1028509503@qq.com>
2023-04-20 11:42:11 +0800

    fix

---
+++ js/member.js
@@ -0,0 +1,54 @@
+var member = [
+{
+  "phone": "No.2038",
+  "name": "朱廷果"
+},
+{
+  "phone": "No.2038",
+  "name": "黄旭凯"
+},
+{
+  "phone": "No.2038",
+  "name": "黄银银"
+},
+{
+  "phone": "No.2038",
+  "name": "刘晓雯"
+},
+{
+  "phone": "No.2038",
+  "name": "罗瑞霞"
+},
+{
+  "phone": "No.2038",
+  "name": "唐静"
+},
+{
+  "phone": "No.2038",
+  "name": "王涵"
+},
+{
+  "phone": "No.2038",
+  "name": "吴绍鹏"
+},
+{
+  "phone": "No.2038",
+  "name": "夏雪天"
+},
+{
+  "phone": "No.2038",
+  "name": "姚玲"
+},
+{
+  "phone": "No.2038",
+  "name": "张邱悦"
+},
+{
+  "phone": "No.2038",
+  "name": "张星"
+},
+{
+  "phone": "No.2038",
+  "name": "字永芳"
+},
+]
